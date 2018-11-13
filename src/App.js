cb00c30bcefc2028416879db560418fc23087acc
Vladimir Kopychev <vladimir.kopychev@udemy.com>
2018-11-13 17:27:56 +0000

    Move more UI out of React

--- src/App.js
+++ src/App.js
@@ -10,38 +10,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentStepName: null,
+      currentTooltipName: null,
     };
   }
 
   firstAction = () => {
     setTimeout(() => {
-        this.setState({ currentStepName: 'event' });
+        this.setState({ currentTooltipName: 'event' });
         document.querySelector('#event-message').classList.remove('hidden');
     }, 500);
   }
 
   componentDidMount() {
-    this.setState({ currentStepName: 'welcome' });
+    this.setState({ currentTooltipName: 'welcome' });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.currentStepName === 'welcome' && this.state.currentStepName === null) {
-        return this.setState({ currentStepName: 'first_action' });
+    if (prevState.currentTooltipName === 'welcome' && this.state.currentTooltipName === null) {
+        return this.setState({ currentTooltipName: 'first_action' });
     }
   }
 
     onTooltipClose = () => {
         this.setState({
-            currentStepName: null,
+            currentTooltipName: null,
         });
     }
 
   render() {
     return (
       <div className="app">
-          <Homepage currentStepName={ this.state.currentStepName } onClick={ this.firstAction } />
-          <TooltipManager currentTooltip={ this.state.currentStepName } onTooltipClose={ this.onTooltipClose } />
+          <Homepage currentTooltipName={ this.state.currentTooltipName } onClick={ this.firstAction } />
+          <TooltipManager currentTooltip={ this.state.currentTooltipName } onTooltipClose={ this.onTooltipClose } />
       </div>
     );
   }
